97473b76dae35b3926c9017bdca3d0049abfcc08
aswinkvncl <a.kambian-veettil2@newcastle.ac.uk>
2021-11-14 17:49:43 +0000

    customer reg tests

--- src/test/java/org/jboss/quickstarts/wfk/booking/CustomerRestServiceTest.java
+++ src/test/java/org/jboss/quickstarts/wfk/booking/CustomerRestServiceTest.java
@@ -4,6 +4,9 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.quickstarts.wfk.booking.model.Customer;
+import org.jboss.quickstarts.wfk.contact.Contact;
+import org.jboss.quickstarts.wfk.contact.UniqueEmailException;
+import org.jboss.quickstarts.wfk.util.RestServiceException;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -16,7 +19,7 @@ import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.io.File;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * <p>A suite of tests, run with {@link org.jboss.arquillian Arquillian} to test the JAX-RS endpoint for
@@ -58,16 +61,78 @@ public class CustomerRestServiceTest {
     CustomerRestService customerRestService;
 
     @Test
-    @InSequence(4)
+    @InSequence(1)
     public void testRegisterCustomer() throws Exception {
-        Customer customer = new Customer();
-        customer.setEmail("john@doe.com");
-        customer.setFirstName("John");
-        customer.setLastName("Doe");
-        customer.setPhoneNumber("(212) 555-1234");
+        Customer customer = createCustomerInstance("john1@doe.com", "John", "Doe", "(212) 555-1234");
         Response response = customerRestService.createCustomer(customer);
 
         assertEquals("Unexpected response status", 201, response.getStatus());
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    @InSequence(2)
+    public void testInvalidRegister() {
+        Customer customer = createCustomerInstance("", "", "", "");
+        try {
+            customerRestService.createCustomer(customer);
+            fail("Expected a RestServiceException to be thrown");
+        } catch(RestServiceException e) {
+            assertEquals("Unexpected response status", Response.Status.BAD_REQUEST, e.getStatus());
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @InSequence(3)
+    public void testDuplicateEmail() throws Exception {
+        // Register an initial user
+        Customer customer = createCustomerInstance("jane2@doe.com","Jane", "Doe", "(212) 555-1234");
+        customerRestService.createCustomer(customer);
+
+        // Register a different user with the same email
+        Customer anotherCustomer = createCustomerInstance("jane2@doe.com", "John", "Doe", "(213) 355-1234");
+
+        try {
+            customerRestService.createCustomer(anotherCustomer);
+            fail("Expected a RestServiceException to be thrown");
+        } catch(RestServiceException e) {
+            assertEquals("Unexpected response status", Response.Status.CONFLICT, e.getStatus());
+            assertTrue("Unexpected error. Should be Unique email violation", e.getCause() instanceof UniqueEmailException);
+            assertEquals("Unexpected response body", 1, e.getReasons().size());
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @InSequence(4)
+    public void testFindCustomer() throws Exception {
+        Customer customer = createCustomerInstance("jane3@doe.com","Jane", "Doe", "(212) 555-1234");
+        customerRestService.createCustomer(customer);
+
+        Response res = customerRestService.retrieveCustomerById(customer.getId());
+        Customer c = (Customer)res.getEntity();
+        assertEquals(200, res.getStatus());
+        assertEquals(c.getId(), customer.getId());
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @InSequence(5)
+    public void testFindAllCustomer() throws Exception {
+        Response res = customerRestService.retrieveAllCustomers(null, null);
+        assertEquals(200, res.getStatus());
+    }
+
+    private Customer createCustomerInstance(String email, String fName, String lName, String phoneNo) {
+        Customer customer = new Customer();
+        customer.setEmail(email);
+        customer.setFirstName(fName);
+        customer.setLastName(lName);
+        customer.setPhoneNumber(phoneNo);
+        return customer;
+    }
 }
